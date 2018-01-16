66afe910c2f4b4be14575ea68e62429d73b1092e
JakubWienzek <contact@jakubwienzek.eu>
2018-01-16 17:35:07 +0100

    All done and styled

--- src/app.js
+++ src/app.js
@@ -3,6 +3,23 @@ const context = canvas.getContext('2d');
 
 context.scale(20, 20);
 
+function arenaSweep() {
+    let rowCount = 1;
+    outer: for (let y = arena.length - 1; y > 0; --y) {
+        for(let x = 0; x <arena[y].length; ++x) {
+            if(arena[y][x]===0) {
+                continue outer;
+            }
+        }
+        const row = arena.splice(y, 1)[0].fill(0);
+        arena.unshift(row);
+        ++y;
+
+        player.score += rowCount *10;
+        rowCount *= 2;
+    }
+}
+
 function collide(arena, player) {
     const [m, o] = [player.matrix, player.pos];
     for(let y = 0; y < m.length; ++y) {
@@ -34,38 +51,38 @@ function createPiece(type) {
         ];
     } else if (type === "O") {
         return [
-            [1, 1],
-            [1, 1],
+            [2, 2],
+            [2, 2],
         ];
     } else if (type === "L") {
         return [
-            [0, 1, 0],
-            [0, 1, 0],
-            [0, 1, 1]
+            [0, 3, 0],
+            [0, 3, 0],
+            [0, 3, 3]
         ];
     } else if (type === "J") {
         return [
-            [0, 1, 0],
-            [0, 1, 0],
-            [1, 1, 0]
+            [0, 4, 0],
+            [0, 4, 0],
+            [4, 4, 0]
         ];
     } else if (type === "I") {
         return [
-            [0, 1, 0, 0],
-            [0, 1, 0, 0],
-            [0, 1, 0, 0],
-            [0, 1, 0, 0],
+            [0, 5, 0, 0],
+            [0, 5, 0, 0],
+            [0, 5, 0, 0],
+            [0, 5, 0, 0],
         ];
     } else if (type === "S") {
         return [
-            [0, 1, 1],
-            [1, 1, 0],
+            [0, 6, 6],
+            [6, 6, 0],
             [0, 0, 0]
         ];
     } else if (type === "Z") {
         return [
-            [1, 1, 0],
-            [0, 1, 1],
+            [7, 7, 0],
+            [0, 7, 7],
             [0, 0, 0]
         ];
     }
@@ -83,7 +100,7 @@ function drawMatrix(matrix, offset) {
     matrix.forEach((row, y) => {
         row.forEach((value, x) => {
             if(value !== 0) {
-                context.fillStyle = "red";
+                context.fillStyle = colors[value];
                 context.fillRect(x + offset.x,
                                 y + offset.y , 
                                 1, 1);
@@ -108,6 +125,8 @@ function playerDrop() {
         player.pos.y--;
         merge(arena, player);
         playerReset();
+        arenaSweep();
+        updateScore();
     }
     dropCounter = 0;
 }
@@ -128,6 +147,8 @@ function playerReset() {
 
     if(collide(arena, player)) {
         arena.forEach(row => row.fill(0));
+        playerRotate.score = 0;
+        updateScore();
     }
 }
 
@@ -187,15 +208,30 @@ function update(time = 0) {
     requestAnimationFrame(update);
 }
 
+function updateScore() {
+    document.getElementById("score").innerText = player.score;
+}
+
 const arena = createMatrix(12, 20);
 //console.log(arena);
 //console.table(arena);
 
 const player = {
-    pos: {x: 5, y:5},
-    matrix: createPiece('T'),
+    pos: {x: 0, y:0},
+    matrix: null,
+    score: 0,
 }
 
+const colors = [
+    null,
+    'red',
+    'blue',
+    'purple',
+    'green',
+    'pink',
+    'yellow',
+    'orange',
+]
 
 document.addEventListener('keydown', event => {
     if (event.keyCode === 37) {
@@ -211,6 +247,8 @@ document.addEventListener('keydown', event => {
     }
 })
 
+updateScore();
+playerReset();
 update();
 
 
